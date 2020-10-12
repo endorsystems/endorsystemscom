d30679d8842b2662b2c5bd570477d7612bd6934a
endorsystems <esane.demay@gmail.com>
2020-10-12 10:11:13 -0400

    updated settings.py

--- endorsystemscom/settings.py
+++ endorsystemscom/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'endorsystemscom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '3306',
+        'NAME': 'django',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -109,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
